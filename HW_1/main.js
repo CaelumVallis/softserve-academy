16282034a2384bd281795b0fc98a846e01b5fec2
CaelumVallis <CaelumVallis@users.noreply.github.com>
2021-04-15 00:39:09 +0300

    feat: done all tasks

---
+++ HW_1/main.js
@@ -0,0 +1,25 @@
+const task1 = (L, W, H) => Math.ceil(((L + W) * H * 2) / 16);
+
+//console.log(task1(10, 5, 3));
+
+const task2 = (A1, A2, A3, B1, B2, B3) => {
+	const sortedSand = [A1, A2, A3].sort((a, b) => b - a);
+	const sortedBoxes = [B1, B2, B3].sort((a, b) => b - a);
+	return sortedBoxes.reduce((acc, item, index) => (acc += item * sortedSand[index]), 0);
+};
+
+//console.log(task2(777, 228, 4, 2, 3, 4));
+
+const task3 = (ticket) => {
+	const ticketArr = ticket.toString().split('');
+	const arr = new Array(6).fill('0');
+	arr.forEach((item, index) => {
+		arr[index] = ticketArr[index] || '0';
+	});
+	const firstSum = arr.slice(0, 3).reduce((acc, item) => acc + Number(item), 0);
+	const secondSum = arr.slice(3, 6).reduce((acc, item) => acc + Number(item), 0);
+	if (firstSum === secondSum) return 'YES';
+	return 'NO';
+};
+
+// console.log(task3(123321));
